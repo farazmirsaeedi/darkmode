10c6371aa1d41023e28a6c38493dd1bd48566546
farazmirsaeedi <farazmirsaeedi@gmail.com>
2023-12-20 00:00:03 +0330

    fix bug

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 let root = document.documentElement,
 checkbox = document.getElementById("dark");
 
-
-
 checkbox.addEventListener("input", function () {
-    root.className.toggle("dark")
-});
+    root.classList.toggle("dark");
+})
